79dfc76791ac26535289ad43ab2a28c5a27b3adc
Eidan <Eidandreammer@gmail.com>
2025-12-04 16:31:38 -0500

    proceso de definicion de endopints para postgreSQL

--- index.js
+++ index.js
@@ -17,6 +17,50 @@ server.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+server.get("/usuarios", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM usuarios");
+    res.status(200).json({
+      data: result.rows,
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error retrieving users",
+    });
+  }
+});
+
+server.get("/usuarios/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  try {
+    const result = await pool.query("SELECT * FROMM usuarios WHERE id = $1", [
+      1,
+    ]);
+    const usuario = result.rows[0];
+
+    if (!usuario) {
+      res.status(404).json({
+        success: false,
+        message: "Usuario no encontrado",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Usuario encontrado",
+    });
+  } catch (error) {
+    console.error("Error al obtener usuario");
+    res.status(500).json({
+      message: "Error interno del servidor",
+      success: false,
+    });
+  }
+});
+
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
