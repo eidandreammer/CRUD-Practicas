7caa1002801b79a1604d8658bb5bbf961c002a34
Eidan <Eidandreammer@gmail.com>
2025-12-04 18:16:19 -0500

    metodo get, post, put and delete creados y funcionando

--- index.js
+++ index.js
@@ -19,47 +19,90 @@ server.get("/", (req, res) => {
 
 server.get("/usuarios", async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM usuarios");
+    const usuarios = await pool.query("SELECT * FROM usuarios ORDER BY id ASC");
+
+    if (!usuarios.rows.length) {
+      res.status(404).json({
+        message: "Usuarios no encontrados",
+        success: false,
+      });
+      return;
+    }
+
     res.status(200).json({
-      data: result.rows,
       success: true,
+      data: usuarios.rows,
     });
   } catch (error) {
+    console.error("Error del servidor");
     res.status(500).json({
+      message: "Error del lado del servidor",
       success: false,
-      message: "Error retrieving users",
     });
   }
 });
 
 server.get("/usuarios/:id", async (req, res) => {
   const id = Number(req.params.id);
-
   try {
-    const result = await pool.query("SELECT * FROMM usuarios WHERE id = $1", [
-      1,
+    const resultado = await pool.query("SELECT * FROM usuarios WHERE id=$1", [
+      id,
     ]);
-    const usuario = result.rows[0];
-
+    const usuario = resultado.rows[0];
     if (!usuario) {
       res.status(404).json({
-        success: false,
         message: "Usuario no encontrado",
+        success: false,
       });
+      return;
     }
 
     res.status(200).json({
+      data: usuario,
       success: true,
-      message: "Usuario encontrado",
     });
   } catch (error) {
-    console.error("Error al obtener usuario");
+    console.error("Error del lado del servidor");
     res.status(500).json({
-      message: "Error interno del servidor",
+      message: "Error del lado del servidor",
       success: false,
     });
   }
 });
+
+server.post("/usuarios", async (req, res) => {
+  const { name, age } = req.body;
+
+  if (name === undefined || age === undefined) {
+    res.status(400).json({
+      success: false,
+      message: "Falta informacion",
+    });
+    return;
+  }
+
+  try {
+    const query =
+      "INSERT INTO usuarios (name, age) VALUES ($1, $2) RETURNING *;";
+    const values = [name, age || null];
+
+    const result = await pool.query(query, values);
+    const nuevoUsuario = result.rows[0];
+
+    res.status(201).json({
+      data: nuevoUsuario,
+      success: true,
+    });
+  } catch (error) {
+    console.error("Error al crear usuario", error);
+    res.status(500).json({
+      success: false,
+      message: "Error al crear el usuario",
+    });
+  }
+});
+
+
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
